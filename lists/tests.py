4d8566f75a74898c43b90c8bb0279cfbac9f9c0f
eailfly <eailfly@gmail.com>
2020-02-14 17:16:53 +0800

    Split urls for lists from the main one.

--- lists/tests.py
+++ lists/tests.py
@@ -23,7 +23,7 @@ class HomePageTest(TestCase):
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
         list_ = List.objects.create()
-        response = self.client.get(f'/lists/{list_.id}/')
+        response = self.client.get(f'/lists/{list_.id}')
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_only_items_for_that_list(self):
@@ -34,7 +34,7 @@ class ListViewTest(TestCase):
         Item.objects.create(text='Other list item 1', list=other_list)
         Item.objects.create(text='Other list item 2', list=other_list)
 
-        response = self.client.get(f'/lists/{correct_list.id}/')
+        response = self.client.get(f'/lists/{correct_list.id}')
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
@@ -44,7 +44,7 @@ class ListViewTest(TestCase):
     def test_passes_correct_list_to_templage(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
-        response = self.client.get(f'/lists/{correct_list.id}/')
+        response = self.client.get(f'/lists/{correct_list.id}')
         self.assertEqual(response.context['list'], correct_list)
 
 
@@ -59,7 +59,7 @@ class NewListTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
         new_list = List.objects.first()
-        self.assertRedirects(response, f'/lists/{new_list.id}/')
+        self.assertRedirects(response, f'/lists/{new_list.id}')
 
 
 class NewItemTest(TestCase):
@@ -84,7 +84,7 @@ class NewItemTest(TestCase):
             f'/lists/{correct_list.id}/add_item',
             data={'item_text': 'A new item for an existing list'})
 
-        self.assertRedirects(response, f'/lists/{correct_list.id}/')
+        self.assertRedirects(response, f'/lists/{correct_list.id}')
 
 
 class ListAndItemModelTest(TestCase):
