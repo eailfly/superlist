9c99bf60a32c318279c317614be61382200d9d65
eailfly <eailfly@gmail.com>
2020-02-24 15:09:08 +0800

    Move unit tests into a directory with single file

--- lists/tests/tests_all.py
+++ lists/tests/tests_all.py
@@ -1,8 +1,8 @@
 from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
-from .views import home_page
-from .models import Item, List
+from lists.views import home_page
+from lists.models import Item, List
 
 
 # Create your tests here.
