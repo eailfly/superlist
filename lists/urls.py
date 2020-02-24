8ec5eb5dcbd8b686674902105e666fb6a694221a
eailfly <eailfly@gmail.com>
2020-02-24 16:11:49 +0800

    Refactor list iew to handle new item POSTs

--- lists/urls.py
+++ lists/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.urls import path
 
-from .views import new_list, view_list, add_item
+from .views import new_list, view_list
 
 urlpatterns = [
     path('new', new_list, name='new_list'),
     path('<int:list_id>', view_list, name='view_list'),
-    path('<int:list_id>/add_item', add_item, name='add_item'),
 ]
